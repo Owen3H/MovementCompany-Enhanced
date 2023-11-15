11eabd876a9f974527f60bf01ef4859b13f952b7
O.S <owen77stubbs@gmail.com>
2023-11-15 20:17:34 +0000

    take out commented code

--- Component/MovementAdder.cs
+++ Component/MovementAdder.cs
@@ -6,29 +6,18 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace MovementCompany.Component
-{
-    internal class MovementAdder : MonoBehaviour
-    {
-        public void Update()
-        {
+namespace MovementCompany.Component {
+    internal class MovementAdder : MonoBehaviour {
+        public void Update() {
             PlayerControllerB[] players = GameObject.FindObjectsOfType<PlayerControllerB>();
 
-            foreach (PlayerControllerB player in players)
-            {
-                if (player != null)
-                {
-                    if (player.gameObject.GetComponentInChildren<MovementScript>() == null)
-                    {
-                        if (player.IsOwner && player.isPlayerControlled)
-                        {
-                            player.gameObject.AddComponent<MovementScript>().myPlayer = player;
-                            Plugin.Log.LogMessage("Gave player the movement script");
-                        }
-                        else
-                        {
-                            
-                        }
+            foreach (PlayerControllerB player in players) {
+                if (player == null) return
+                
+                if (player.gameObject.GetComponentInChildren<MovementScript>() == null) {
+                    if (player.IsOwner && player.isPlayerControlled) {
+                        player.gameObject.AddComponent<MovementScript>().myPlayer = player;
+                        Plugin.Log.LogMessage("Gave player the movement script");
                     }
                 }
             }
--- Component/MovementScript.cs
+++ Component/MovementScript.cs
@@ -6,10 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace MovementCompany.Component
-{
-    internal class MovementScript : MonoBehaviour
-    {
+namespace MovementCompany.Component {
+    internal class MovementScript : MonoBehaviour {
         public PlayerControllerB myPlayer;
         bool inAir;
         public static Vector3 wantedVelToAdd;
@@ -17,37 +15,36 @@ namespace MovementCompany.Component
 
         private Vector3 previousForward;
         
-
-        public void Update()
-        {
-            if (myPlayer.playerBodyAnimator.GetBool("Jumping") && jumpTime < 0.1f)
-            {
+        public void Update() {
+            if (myPlayer.playerBodyAnimator.GetBool("Jumping") && jumpTime < 0.1f) {
                 myPlayer.fallValue = myPlayer.jumpForce;
                 jumpTime += Time.deltaTime * 10f;
             }
+
             myPlayer.sprintMeter = 100;
+
             if (!myPlayer.thisController.isGrounded && !myPlayer.isClimbingLadder)
             {
-                if (!inAir)
-                {
+                if (!inAir) {
                     inAir = true;
+
                     Vector3 vel = myPlayer.thisController.velocity;
                     vel.y = 0;
+                    
                     wantedVelToAdd += 0.006f * vel;
                 }
-                wantedVelToAdd.y = 0;
-                myPlayer.thisController.Move(myPlayer.gameObject.transform.forward * wantedVelToAdd.magnitude);
-                //Plugin.Log.LogMessage("vel");
 
                 Vector3 currentForward = myPlayer.gameObject.transform.forward;
-                Vector3 forwardChange = currentForward - previousForward;
+                
+                wantedVelToAdd.y = 0;
+                myPlayer.thisController.Move(currentForward * wantedVelToAdd.magnitude);
 
+                Vector3 forwardChange = currentForward - previousForward;
                 float rotationThreshold = 0.01f;
 
                 if (forwardChange.magnitude > rotationThreshold)
-                {
                     wantedVelToAdd += new Vector3(0.0005f, 0.0005f, 0.0005f);
-                }
+
                 previousForward = currentForward;
             }
             else
@@ -56,13 +53,6 @@ namespace MovementCompany.Component
                 inAir = false;
                 jumpTime = 0;
             }
-
-            if (myPlayer.thisController.isGrounded)
-            {
-                //Plugin.Log.LogMessage("vel");
-            }
-
-            
         }
     }
 }
