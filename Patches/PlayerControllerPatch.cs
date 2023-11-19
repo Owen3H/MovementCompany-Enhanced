22fbb964e9a3a3d5a3d9342a3d46839b0cb1a098
O.S <owen77stubbs@gmail.com>
2023-11-19 22:27:22 +0000

    Update PlayerControllerPatch.cs

--- Patches/PlayerControllerPatch.cs
+++ Patches/PlayerControllerPatch.cs
@@ -6,10 +6,9 @@ using UnityEngine;
 namespace MovementCompany.Patches {
     [HarmonyPatch(typeof(PlayerControllerB))]
     class PlayerControllerPatch {
-        [HarmonyPrefix]
         [HarmonyPatch("PlayerJump")]
-        public static IEnumerator OverridePlayerJump(IEnumerable result) {
-            foreach(var e in result) {
+        public static IEnumerator Postfix(IEnumerator __result) {
+            foreach(var e in __result) {
                 if (e is not WaitForSeconds) {
                     yield return e;
                 }
